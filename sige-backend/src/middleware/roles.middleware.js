100475e0e462fd5a4a481d055ce1e5780d1c0b80
Carlos060801 <71476055+Carlos060801@users.noreply.github.com>
2025-12-08 12:43:21 -0500

    actualización backend
    actualización backend

--- sige-backend/src/middleware/roles.middleware.js
+++
@@ -1,8 +0,0 @@
-export const allowRoles = (...rolesPermitidos) => {
-  return (req, res, next) => {
-    if (!rolesPermitidos.includes(req.user.role)) {
-      return res.status(403).json({ message: "No tienes permisos para realizar esta acción" });
-    }
-    next();
-  };
-};
--- sige-backend/src/utils/fechaBogota.js
+++
@@ -1,6 +0,0 @@
-// utils/fechaBogota.js
-export const fechaBogota = () => {
-  const ahoraUTC = new Date();
-  const bogota = new Date(ahoraUTC.getTime() - (5 * 60 * 60 * 1000));
-  return bogota;
-};
